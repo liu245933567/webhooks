7e2f5d0502611a0199c9a08afad9c3df8e2c06ca
liu245933567 <245933567@qq.com>
2022-07-05 10:13:31 +0800

    添加 blog 发版脚本

---
+++ blog.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+WORK_PATH='/root/my-product/webhooks'
+DIST_PATH='/root/my-product/webhooks/dist'
+NGINX_HTML='/www/server/nginx/html'
+
+cd $WORK_PATH
+echo "先清除老代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取新代码"
+git pull origin master
+echo "下载第三方包"
+pnpm install
+echo "编译build"
+pnpm build
+echo "删除nginx静态资源"
+cd $NGINX_HTML
+rm -rf *
+echo "移动打包后的静态资源"
+cp -r $DIST_PATH/* ../html/
